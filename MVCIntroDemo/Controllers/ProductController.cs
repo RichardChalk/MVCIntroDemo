75e9f60aef1aa26affd40b2fdc0423da1420f69b
Richard Chalk <barmybrit@gmail.com>
2023-02-19 20:49:35 +0100

    Final

--- MVCIntroDemo/Controllers/ProductController.cs
+++ MVCIntroDemo/Controllers/ProductController.cs
@@ -4,9 +4,31 @@ namespace MVCIntroDemo.Controllers
 {
     public class ProductController : Controller
     {
+        public class ProductViewModel
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Price { get; set; }
+            public int Qty { get; set; }
+        }
+        public List<ProductViewModel> Products { get; set; } = new List<ProductViewModel>();
         public IActionResult Index()
         {
-            return View();
+            Products.Add(new ProductViewModel{ 
+                Id = 1, 
+                Name = "XBox", 
+                Price = 1500, 
+                Qty = 10 
+            });
+            Products.Add(new ProductViewModel
+            {
+                Id = 2,
+                Name = "PS5",
+                Price = 5000,
+                Qty = 100
+            });
+
+            return View(Products);
         }
     }
 }
